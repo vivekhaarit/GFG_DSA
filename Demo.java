7f2e5e4eee881bd027fc748c510d2e152865fd6a
vivekhaarit <vivekhaarit@gmail.com>
2023-01-18 23:28:38 +0530

    digits in factorial for small numbers

--- Demo.java
+++ Demo.java
@@ -1,5 +1,30 @@
+
+import java.util.Scanner;
+
 public class Demo {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("sldkf");
+        Solution obj = new Solution();
+        System.out.print("Enter any number: ");
+        int n = scanner.nextInt();
+
+        int result = obj.digitsInFactorial(n);
+        System.out.println("Digits in factorial : "+result);
+    }
+
+}
+
+class Solution{
+    public int digitsInFactorial(int N){
+        int  factorial =1;
+        for(int i=1;i<=N;i++){
+            factorial *= i;
+        }
+        int count =1;
+        while(factorial != 0){
+            factorial = factorial % 10;
+            count++;
+        }
+        return count;
     }
 }
